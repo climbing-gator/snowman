ed86c92f60bcf33fa97458220664bc83ace1f38d
Michelle <bereczgator@gmail.com>
2021-02-08 20:36:36 -0600

    create projs and sln, stub out snowman class

---
+++ source/SnowmanBody.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowman
+{
+    class SnowmanBody
+    {
+        private string[] bodyParts = new string[] { "bottom", "middle", "head", "left arm", "right arm", "left eye", "right eye", "mouth", "nose" };
+        private int numberOfSnowmanParts = 0;
+        
+        public void AddBodyPart() 
+        {
+            numberOfSnowmanParts++;
+        }
+
+        public bool IsSnowmanComplete()
+        {
+            if (numberOfSnowmanParts == bodyParts.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
